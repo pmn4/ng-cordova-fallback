44c1b8ecbdadb79f5cabb09b26ad31e27fa2d41f
Pat Newell <pnewell+git@renttherunway.com>
2015-02-08 14:05:51 -0500

    Network: Sometimes ngCordovaNetwork is Available Browsers
    ... so this code ensures that the `Connection` object, defined in the plugin code, is available

--- src/plugins/network.js
+++ src/plugins/network.js
@@ -1,3 +1,16 @@
+// ngNetwork works on mobile event in the absence of the plugin, but Connection may be missing.
+// https://github.com/apache/cordova-plugin-network-information/blob/master/www/Connection.js
+window.Connection = window.Connection || {
+	UNKNOWN: "unknown",
+	ETHERNET: "ethernet",
+	WIFI: "wifi",
+	CELL_2G: "2g",
+	CELL_3G: "3g",
+	CELL_4G: "4g",
+	CELL:"cellular",
+	NONE: "none"
+};
+
 angular
 	.module("ngCordovaFallback.plugins.network", [])
 	.factory("$cordovaFallbackNetwork", [
